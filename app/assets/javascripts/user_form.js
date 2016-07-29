74012e22e0c5f2ab60f5deacd5b701f3bf87672b
israel.hill <ihilljr@us.ibm.com>
2016-07-29 12:33:55 -0400

    added submit button to form. created js file to control form

---
+++ app/assets/javascripts/user_form.js
@@ -0,0 +1,22 @@
+var UserForm = (function() {
+    var userFirstName;
+    var userLastName;
+    var userPhone;
+    var userEmail;
+
+    var childFirstName;
+    var childLastName;
+    var childPhone;
+
+    var drugName_1;
+    var drugDosage_1;
+    var drugFreq_1;
+
+    var drugName_2;
+    var drugDosage_2;
+    var drugFreq_2;
+
+    var drugName_3;
+    var drugDosage_3;
+    var drugFreq_3;
+});
