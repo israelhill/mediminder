59fb77d32a70c4a61ccc943697ed38a1d164514d
Apoorva Havanur <ahavanu@us.ibm.com>
2016-07-29 13:05:45 -0400

    the rest of the basic messages

--- Message/send-sms.rb
+++ Message/send-sms.rb
@@ -3,8 +3,7 @@ require 'twilio-ruby'
 
 # put your own credentials here
 account_sid = 'ACee177252e5f1e4a5c4c19b8e1e10d5d9'
-auth_token = '
-92760c86abd82c59035d16709b99ee8c'
+auth_token = '92760c86abd82c59035d16709b99ee8c'
 FROM_NUMBER = '+17347864530'
 TO_NUMBER = '+17343550556'
 
@@ -38,4 +37,46 @@ def send_negation_response_message(name, phone, caregiverFirstName, caregiverLas
 
 end
 
+def send_dosage_message(drug, dosage, phone, frequency)
+  @client.account.messages.create({ :from => FROM_NUMBER,
+                                    :to => phone,
+                                    :body => 'You usually take %d %s pills %d a day' %[dosage, drug, frequency]
+                                  })
+end
+
+def send_frequency_message(drug, phone, frequency)
+  @client.account.messages.create({ :from => FROM_NUMBER,
+                                    :to => phone,
+                                    :body => 'You usually take your %s medicine %d a day' %[drug, frequency]
+                                  })
+end
+
+def send_frequency_message(drug, phone, frequency)
+  @client.account.messages.create({ :from => FROM_NUMBER,
+                                    :to => phone,
+                                    :body => 'You usually take your %s medicine %d a day' %[drug, frequency]
+                                  })
+end
+
+def send_side_effect_list_message(drug, sideeffects)
+  @client.account.messages.create({ :from => FROM_NUMBER,
+                                    :to => phone,
+                                    :body => 'The typical side effects of %s are $s. If you are experiencing any of these symptoms, tell your caregiver right away' %[drug, sideeffects]
+                                  })
+end
+
+def send_side_effect_cause_message(drug, sideeffect, flag)
+  if (flag)
+    @client.account.messages.create({ :from => FROM_NUMBER,
+                                      :to => phone,
+                                      :body => 'yes it looks like %s can $s. If you are experiencing %s ask your caregiver to let your doctor know right away' % [drug, sideeffect]
+                                    })
+  else
+    @client.account.messages.create({ :from => FROM_NUMBER,
+                                      :to => phone,
+                                      :body => 'no %s does not seem to cause %s. Talk to your caregiver about visiting the doctor for more help' % [drug, sideeffect]
+                                    })
+
+  end
+end
 
