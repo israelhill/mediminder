c808bb41c49b643679014fb65ef22ed13b6ee125
israel.hill <ihilljr@us.ibm.com>
2016-07-31 16:15:36 -0400

    not using secrets.yml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,11 @@ class MessagesController < ApplicationController
   skip_before_filter :authenticate_user!, :only => "reply"
 
   def reply
+    message_body = params['Body']
+    from_number = params['From']
     boot_twilio
-    message_body = params["Body"]
-    from_number = params["From"]
-    boot_twilio
-    sms = @client.messages.create(
-        from: Rails.application.secrets.twilio_number,
+    sms = @client.account.messages.create(
+        from: '+17347864530',
         to: from_number,
         body: "Hello there, thanks for texting me. Your number is #{from_number}."
     )
@@ -18,10 +17,9 @@ class MessagesController < ApplicationController
   end
 
   private
-
   def boot_twilio
-    account_sid = Rails.application.secrets.twilio_sid
-    auth_token = Rails.application.secrets.twilio_token
+    account_sid = 'ACee177252e5f1e4a5c4c19b8e1e10d5d9'
+    auth_token = '92760c86abd82c59035d16709b99ee8c'
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 end
